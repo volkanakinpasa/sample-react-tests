3438bfa1ea90817ad32828ea68ae53f691b831da
volkanakinpasa <volkanakintr@gmail.com>
2021-11-16 13:37:09 +0100

    update package name

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
-// import { isEmpty } from 'lodash';
-
-// export const Input = ({ onChange }) => {
-//   const [value, setValue] = React.useState('1');
-//   const formatAndUpdateValue = React.useCallback((e) => {
-//     const { value } = e.target;
-//     const boundedValue = isEmpty(value.trim()) ? '1' : value;
-//     setValue(boundedValue);
-//     onChange(Number(boundedValue));
-//   }, []);
-
-//   return <input onChange={formatAndUpdateValue} value={value} type="text" />;
-// };
 
 export function App() {
   return (
     <div className="App">
-      <h1>Welcome to Enzyme-Hooks testing practice</h1>
-      <Counter initialValue={10} />
+      <h1>Welcome to React testing</h1>
+      <Counter initialValue={0} />
     </div>
   );
 }
